94bf7480b2c0760c99f0b2f36722bab77f9026bd
Klemen Kosir <klemen.kosir@icloud.com>
2017-01-22 17:46:03 +0100

    Initial Commit

---
+++ ChatKit/ChatKit.h
@@ -0,0 +1,19 @@
+//
+//  ChatKit.h
+//  ChatKit
+//
+//  Created by Klemen Kosir on 22. 01. 17.
+//  Copyright © 2017 Klemen Kosir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatKit.
+FOUNDATION_EXPORT double ChatKitVersionNumber;
+
+//! Project version string for ChatKit.
+FOUNDATION_EXPORT const unsigned char ChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatKit/PublicHeader.h>
+
+
